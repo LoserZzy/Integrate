7a642dc7f76718736d8796d2e309cc7d2260d977
Zzy_Main_Computer <1011030023@qq.com>
2021-01-15 14:44:28 +0800

    测试

--- collect/get_additional_hwinfo.c
+++ collect/get_additional_hwinfo.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "get_additional_hwinfo.h"
 
-int main()
+void get_additional_hwinfo()
 {
-    system("./lshw -class cpu > cpuinfo");
-    system("./lshw -class network > networkinfo");
-    system("./lshw -class memory > memoryinfo");
-    system("./lshw -class disk > diskinfo");
-    system("./lshw -class memory > biosinfo");
+    char doit[100];
+    sprintf(doit,"%s%s%s","./lshw -class cpu > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_CPUINFO);
+    system(doit);
+
+    sprintf(doit,"%s%s%s","./lshw -class cpu > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_NETINFO);
+    system(doit);
+
+    sprintf(doit,"%s%s%s","./lshw -class cpu > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_MEMINFO);
+    system(doit);
+
+    sprintf(doit,"%s%s%s","./lshw -class cpu > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_CDISKINFO);
+    system(doit);
+
+    sprintf(doit,"%s%s%s","./lshw -class cpu > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_BIOSINFO);
+    system(doit);
 }
--- collect/get_additional_hwinfo.h
+++ collect/get_additional_hwinfo.h
@@ -1,4 +1,12 @@
 #ifndef GET_ADDITIONAL_HWINFO
 #define GET_ADDITIONAL_HWINFO
-#define SAVE_ADDITIONAL_INFO_PATH "Sys_info.txt"
-#endif GET_ADDITIONAL_HWINFO
+#define SAVE_ADDITIONAL_INFO_DIR_PATH "./result/additional_info/" //其它未收集齐的补充信息（朱正禹的部分）
+#define SAVE_ADDITIONAL_CPUINFO "cpuinfo"
+#define SAVE_ADDITIONAL_NETINFO "networkinfo"
+#define SAVE_ADDITIONAL_MEMINFO "memoryinfo"
+#define SAVE_ADDITIONAL_CDISKINFO "diskinfo"
+#define SAVE_ADDITIONAL_BIOSINFO "biosinfo"
+
+void get_additional_hwinfo(void);
+
+#endif 
