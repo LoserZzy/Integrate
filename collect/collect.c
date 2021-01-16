12050bd702c78369bb9dfed73252d02e700d04df
Zzy_Main_Computer <1011030023@qq.com>
2021-01-16 16:38:33 +0800

    初步完成

--- collect/collect.c
+++ collect/collect.c
@@ -1,5 +1,5 @@
 #include "HDMstruct.h"
-// #include "FWCConnect.h"
+#include "FWCConnect.h"
 #include "SysInfo.h"
 #include "get_additional_hwinfo.h"
 #include "settings.h"
@@ -10,63 +10,63 @@ int main() {
     int sock;
     char report_msg[50];
     // 初始化连接，报告进度0
-    // FWC_client_init(&sock);
+    FWC_client_init(&sock);
     sprintf(report_msg, "start checking");
-    // FWC_progress_report(sock, 5, 0, report_msg);
+    FWC_progress_report(sock, 5, 0, report_msg);
 
     // 开始获取CPU信息，进度报告5
     cpu_info(SAVE_DIR_PATH);
     sprintf(report_msg, "complete cpu_information checking");
-    // FWC_progress_report(sock, 6, 5, report_msg);
+    FWC_progress_report(sock, 6, 5, report_msg);
 
 
     // 开始获取memory信息，进度报告10
     mem_info(SAVE_DIR_PATH);
     sprintf(report_msg, "complete memory_information checking");
-    // FWC_progress_report(sock, 6, 10, report_msg);
+    FWC_progress_report(sock, 6, 10, report_msg);
 
 
     // 开始获取硬盘信息，进度报告15
     disk_info(SAVE_DIR_PATH);
     sprintf(report_msg, "complete disk_information checking");
-    // FWC_progress_report(sock, 6, 15, report_msg);
+    FWC_progress_report(sock, 6, 15, report_msg);
 
 
     // 开始获取网卡信息，进度报告20
     net_info(SAVE_DIR_PATH);
     sprintf(report_msg, "complete netcard_infomation checking");
-    // FWC_progress_report(sock, 6, 20, report_msg);
+    FWC_progress_report(sock, 6, 20, report_msg);
 
 
     // 开始获取BIOS信息，进度报告25
     bios_info(SAVE_DIR_PATH);
     sprintf(report_msg, "complete BIOS_information checking");
-    // FWC_progress_report(sock, 6, 25, report_msg);
+    FWC_progress_report(sock, 6, 25, report_msg);
 
     // 开始获取硬件信息，进度报告40
     hardware(SAVE_DIR_PATH);
     sprintf(report_msg, "complete hardware checking");
-    // FWC_progress_report(sock, 6, 40, report_msg);
+    FWC_progress_report(sock, 6, 40, report_msg);
 
     // 开始获取驱动信息，进度报告70
     driver(SAVE_DIR_PATH);
     sprintf(report_msg, "complete driver checking");
-    // FWC_progress_report(sock, 6, 70, report_msg);
+    FWC_progress_report(sock, 6, 70, report_msg);
 
     // 开始获取模块信息，进度报告80
     module(SAVE_DIR_PATH);
     sprintf(report_msg, "complete module checking");
-    // FWC_progress_report(sock, 6, 80, report_msg);
+    FWC_progress_report(sock, 6, 80, report_msg);
 
     get_additional_hwinfo();
     sprintf(report_msg, "complete additional info checking");
-    // FWC_progress_report(sock, 6, 90, report_msg);
+    FWC_progress_report(sock, 6, 90, report_msg);
 
     sprintf(report_msg, "end checking");
-    // FWC_progress_report(sock, 5, 100, report_msg);
+    FWC_progress_report(sock, 5, 100, report_msg);
 
     // 关闭连接
-    // FWC_client_recovery(&sock);
+    FWC_client_recovery(&sock);
     
     return 0;
 }
--- collect/get_additional_hwinfo.c
+++ collect/get_additional_hwinfo.c
@@ -37,24 +37,37 @@ char* Int2String(int num,char *str)//10进制
 
 void get_additional_hwinfo()
 {
+    system("arch > sysinfo");
+    FILE *fp=fopen("sysinfo","r");
+    char sys[10], lshw[10];
+    fscanf(fp, "%s", sys);
+    fclose(fp);
+    if (sys[0] == 'x' && sys[1] == '8' && sys[2] == '6'){
+        char lshw_x86[10] = "./lshw_x86";
+        for (int a = 0; a < 10; a += 1)
+            lshw[a] = lshw_x86[a];
+    }
+    system("rm sysinfo");
+
+
     char doit[100];
-    sprintf(doit,"%s%s%s","./lshw -class cpu -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_CPUINFO);
+    sprintf(doit, "%s%s%s%s", lshw, " -class cpu -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_CPUINFO);
     system(doit);
 
-    sprintf(doit,"%s%s%s","./lshw -class network -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_NETINFO);
+    sprintf(doit, "%s%s%s%s", lshw, " -class network -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_NETINFO);
     system(doit);
 
-    sprintf(doit,"%s%s%s","./lshw -class memory -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_MEMINFO);
+    sprintf(doit, "%s%s%s%s", lshw, " -class memory -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_MEMINFO);
     system(doit);
 
-    sprintf(doit,"%s%s%s","./lshw -class disk -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_CDISKINFO);
+    sprintf(doit, "%s%s%s%s", lshw, " -class disk -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_CDISKINFO);
     system(doit);
 
-    sprintf(doit,"%s%s%s","./lshw -class memory -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_BIOSINFO);
+    sprintf(doit, "%s%s%s%s", lshw, " -class memory -json > ", SAVE_ADDITIONAL_INFO_DIR_PATH, SAVE_ADDITIONAL_BIOSINFO);
     system(doit);
 
     system("ls /etc/netplan > lsmsg");
-    FILE *fp=fopen("lsmsg","r");
+    fp=fopen("lsmsg","r");
     char data[100];
     int num = 0;
     char number[10];
